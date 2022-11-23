c6e8833940725dc881a9c1638baab16e73ff807c
sakatokukazuki <19223@toba-cmt.ac.jp>
2022-11-23 21:20:24 +0900

    get data

---
+++ get_data.py
@@ -0,0 +1,70 @@
+import pyautogui 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from time import sleep
+import json
+import os
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+#screen_shot = pyautogui.screenshot() 
+#screen_shot.save('test.png')
+
+class GetData():
+    # 雀魂へ
+    def __init__(self, xpath_list):
+        options = Options()
+        options.add_experimental_option('detach', True)
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.get('https://game.mahjongsoul.com/index.html')
+        self.driver.maximize_window()
+        self.xpath = xpath_list
+        self.driver.implicitly_wait(20)
+
+    # 雀魂ログイン
+    def _login_jantama(self, mail_address, password):
+        # gmailでログイン
+        pyautogui.moveTo(1300, 850, duration=17)
+        pyautogui.click()
+
+        # address 入力
+        self._click(self.xpath["address_box"], interval=1)
+        self._send_keys(self.xpath["address_box"], mail_address, interval=1)
+
+        # 次へクリック
+        self._click(self.xpath["next_button"], interval=1)
+
+        # password 入力
+        self._click(self.xpath["password_box"], interval=1)
+        self._send_keys(self.xpath["password_box"], password, interval=1)
+
+        # 次へクリック
+        self._click('//*[@id="passwordNext"]/div/button/span', interval=1)
+
+        # 最後確認
+        sleep(14)
+        pyautogui.moveTo(1000, 770)
+        pyautogui.click()
+
+    def _send_keys(self, xpath, text, interval=0):
+        output = self.driver.find_element(By.XPATH, xpath)
+        output.send_keys(text)
+        sleep(interval)
+
+    def _click(self, xpath, interval=0):
+        button = self.driver.find_element(By.XPATH, xpath)
+        button.click()
+        sleep(interval)
+
+if __name__ == "__main__":
+
+    with open(f'{PATH}/secret.json', 'r') as f:
+        user_data = json.load(f)
+    
+    # xpathjsonを開く
+    with open(f'{PATH}/xpath.json', 'r') as f:
+        xpath_list = json.load(f)
+
+    get_data = GetData(xpath_list)
+    get_data._login_jantama(user_data["mail_address"], user_data["password"])
